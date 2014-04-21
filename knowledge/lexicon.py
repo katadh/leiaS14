3b2aeade18c526582fcb2f430fbe69ba355f1832
samsok <ksamson8505@yahoo.com>
2014-04-21 13:27:05 -0400

    Hard coded the lexicon structure
    The structure is a Dict(Str: Dict(Str: List(Str))).
    We will set up a text document to store the lexicon for now and a script to handle that.

--- knowledge/lexicon.py
+++ knowledge/lexicon.py
@@ -2,7 +2,21 @@ from ontology import *
 
 # TODO: Kyle, feel free to organize it in your way, e.g. using POS as an intermediate key
 
+"""
 lex = {'I': [Animal],
        'buy' : [Buy, BuyEvent],
        'fish' : [Fish, FishEvent],
        '?' : [QuestionEvent]}
+"""
+
+#TODO: Create a script to read from a text file all of the lexicon entries.
+
+#This is actually really nasty and hardcoding the lexicon like this is tedious. 
+#Regardless, the call is lexicon.lexicon[LEMMA][PoS]. This returns a list of concepts associated with the
+#      provided lemma and pos.
+       
+lexicon = {'I': {'n': ['Animal']},
+           'buy': {'n': ['Buy'],
+                   'v': ['BuyEvent']},
+           'fish': {'n': ['Fish'],
+                    'v': ['FishEvent']}}
