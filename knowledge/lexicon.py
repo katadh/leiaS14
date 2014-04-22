9b41de2edceae0b95729b7101c6df2a0a782f3cf
samsok <ksamson8505@yahoo.com>
2014-04-22 15:27:02 -0400

    Un-commented old lexicon structure
    use lex for old, lexicon for new

--- knowledge/lexicon.py
+++ knowledge/lexicon.py
@@ -2,12 +2,10 @@ from ontology import *
 
 # TODO: Kyle, feel free to organize it in your way, e.g. using POS as an intermediate key
 
-"""
 lex = {'I': [Animal],
        'buy' : [Buy, BuyEvent],
        'fish' : [Fish, FishEvent],
        '?' : [QuestionEvent]}
-"""
 
 #TODO: Create a script to read from a text file all of the lexicon entries.
 
