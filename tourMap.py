66e6b0d7c650a05a864a8600e29917e3d898503d
katadh <katadh@sbcglobal.net>
2014-05-01 16:48:52 -0400

    finished code to find path in graph and output directions

--- tourMap.py
+++ tourMap.py
@@ -1,19 +1,60 @@
 import Queue
+import json
 
 # POI = Point of Interest
-class tourPOI:
-    def __init__(self, loc_name, descrip, out):
+class POI:
+    def __init__(self, loc_name, descrip, out, neighbrs):
         self.name = loc_name
         self.description = descrip
         # outside is a boolean indicating if the location is indoors
         # or outdoors
         self.outdoors = out
+        self.neighbors = neighbrs
 
-class tourMap:
-    def __init__(self, poi):
-        # This should be a list of locations
-        self.POI = poi
+class Map:
 
+    def __init__(self, graphfile):
+        self.POI = {}
+        jsondata = open(graphfile)
+        graphdata = json.load(jsondata)
+
+        for node in graphdata:
+           name = node["name"]
+           descrip = node["description"]
+           out = node["outdoors"]
+           neighbors = node["neighbors"]
+           self.POI[name] = POI(name, descrip, out, neighbors)
+
+
+    def giveDirections(self, start, end):
+        paths = self.findPath(start, end)
+
+        path = [end]
+        while paths[path[-1]] != '':
+            path.append(paths[path[-1]])
+
+        path.reverse()
+        condensed_path = []
+
+        prev_direction = ""
+        for i in range(len(path) - 1):
+            direction = [d[2] for d in self.POI[path[i]].neighbors if d[0] == path[i + 1]][0]
+            if direction != prev_direction:
+                condensed_path.append(direction)
+                condensed_path.append(path[i + 1])
+            else:
+                condensed_path[-1] = path[i + 1]
+            prev_direction = direction
+
+        directions = ""
+        for direction, location in zip( condensed_path[::2], condensed_path[1::2]):
+            directions = directions + "go " + direction + " to the " + location + " then "
+            
+        directions = directions + "enjoy!"
+
+        return directions
+            
+        
     # start and end should be strings
     # (the names of locations)
     def findPath(self, start, end):
@@ -21,28 +62,26 @@ class tourMap:
         loc_list = []
         dist = {start : 0}
         parent = {start : ""}
-        for location in self.POI:
-            name = location.name
+        for name, location in self.POI.items():
             if name != start:
                 dist[name] = inf
-                parent[name] = ""
             loc_list.append([dist[name], location])
 
-        while q.empty() != True:
+        while len(loc_list) > 0:
             location = loc_list.pop(loc_list.index(min(loc_list)))
             
-            if dist[location] == inf:
-                return []
+            if dist[location[1].name] == inf:
+                return {}
                 
             for neighbor in location[1].neighbors:
                 test_dist = dist[location[1].name] + neighbor[1]
-                if test_dist < dist[neighbor[0].name]:
-                    dist[neighbor[0].name] = test_dist
-                    prev[neighbor[0].name] = location[1].name
+                if test_dist < dist[neighbor[0]]:
+                    dist[neighbor[0]] = test_dist
+                    parent[neighbor[0]] = location[1].name
                     for i in range(len(loc_list)):
-                        if loc_list[i][1] == neighbor[0].name:
+                        if loc_list[i][1].name == neighbor[0]:
                             loc_list[i][0] = test_dist
-        return [dist, parent]
+        return parent
                     
                     
                     
