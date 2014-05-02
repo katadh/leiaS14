37c17113cfc618fe016cf0da504cb521a39f87d3
cadddr <peter.pampkin@gmail.com>
2014-05-02 19:56:15 -0400

    Moved app logic to plan
    so that the app only handles input and display

--- plan_selection/planList_petrjay.py
+++ plan_selection/planList_petrjay.py
@@ -1,10 +1,30 @@
+current_location = None
+stay_duration = 0
+
+def observe(tmr):
+    global stay_duration
+    print 'stay duration: {0}'.format(stay_duration)
+    stay_duration += 1
+
 def ask_define_location(tmr):
     print 'You seem to be spending quite some time here. What is this place?'
     # expect DefineEvent next
+    
+def on_move(tmr):
+    print 'Oh, I see you moved: {0}'.format(tmr[0])
 
-plan_lexicon = [(set(['StayEvent', 'Location']), 'ask_define_location')]
-plan_map = {'ask_define_location':(1, 1, 0, ask_define_location)}
-plan_map_prereqs = {'ask_define_location': [['', '', None]]}
+
+plan_lexicon = [(set(['AgentWakeEvent']), 'observe'),
+                (set(['StayEvent', 'Location']), 'ask_define_location'),
+                (set(['MoveEvent', 'Location']), 'on_move')]
+
+plan_map = {'observe':(0, -1, 0, observe),
+            'ask_define_location':(1, 1, 0, ask_define_location),
+            'on_move':(1, 1, 0, on_move)}
+
+plan_map_prereqs = {'observe':[[None, None, None]],
+                    'ask_define_location': [[None, None, None]],
+                    'on_move': [[None, None, None]]}
 
 
 
