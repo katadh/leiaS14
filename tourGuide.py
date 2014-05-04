b26b28682f7acf9ab7cebe66fcfb5c41e5cf1d7d
katadh <katadh@sbcglobal.net>
2014-05-04 19:13:44 -0400

    added tourGuide file

---
+++ tourGuide.py
@@ -0,0 +1,16 @@
+import parse
+import leia
+import time
+import knowledge.lexicon_alexnut
+
+def startTourGuide():
+    stan_parse = parse.syntaxParser()
+    time.sleep(3)
+
+    print "How can I help you?\n"
+    while(1):
+        text = raw_input("--> ")
+        if text == "end":
+            return
+        leia.leia(text, lexicon=knowledge.lexicon_alexnut.Lexicon(), sp=stan_parse)
+        
