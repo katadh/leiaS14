f4642027474206603186006378b856fc1a79f2a4
MrGraph <mattklawonn@gmail.com>
2014-05-05 00:03:41 -0400

    Restored functionality to Test.py

--- knowledge/Facts.py
+++ knowledge/Facts.py
@@ -1,5 +1,5 @@
 import inspect
-from ontology_mattkyle import *
+from ontology import *
 
 local_TMRs = {}
 base_TMRs = {}
@@ -12,16 +12,35 @@ def base_store(inst):
         if name not in base_TMRs: base_TMRs[name] = []
         base_TMRs[name].append(inst)
 
-def store(inst):
+def store(inst, sTerm = False):
     classes = inspect.getmro(inst.__class__)
     for c in classes:
         name = c.__name__
         if name == "Concept" or name == "object": continue
         if name not in local_TMRs: local_TMRs[name] = []
-        local_TMRs[name].append(inst)
+        if(sTerm):  local_TMRs[name].insert(0,inst)
+        else:   local_TMRs[name].append(inst)
 
 def load():
-    kbf = open("fact_repo.txt", 'r')
+    m = Chips()
+    store(m)
+    a = Aisle()
+    a.name = 'three'
+    store(a)
+    b = Aisle()
+    b.name = 'four'
+    store(b)
+
+    m.location = a
+    n = Chips()
+    n.location = a
+    store(m)
+    store(n)
+
+    """
+
+    if file == "": return
+    kbf = open(fName, 'r')
     lines = kbf.readlines()
     kbf.close()
     inst_map = {}
@@ -64,10 +83,18 @@ def load():
 
     for k, v in inst_map.items():
         base_store(v)
+    """
         
+def forget(inst):
+    classes = inspect.getmro(inst.__class__)
+    for c in classes:
+        name = c.__name__
+        if name not in local_TMRs: continue
+        else:   local_TMRs[name].remove(inst)
+
 
 def kblookup(kbitem):
     hits = []
-    if kbitem in base_TMRs:  hits.extend(base_TMRs[kbitem])
-    if kbitem in local_TMRs: hits.extend(local_TMRs[kbitem])
+    if kbitem in local_TMRs:  hits.extend(local_TMRs[kbitem])
+    if kbitem in base_TMRs: hits.extend(base_TMRs[kbitem])
     return hits
